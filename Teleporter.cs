7a49861ed2034d6b50f1cf54b5eaa7b4663aae72
devnull-twitch <97809736+devnull-twitch@users.noreply.github.com>
2022-01-20 20:05:16 +0100

    feat: adding teleporter object

--- Teleporter.cs
+++ Teleporter.cs
@@ -12,16 +12,15 @@ public class Teleporter : Area2D
 
     public void NetworkReady()
     {
-        isServer = GetTree().IsNetworkServer();        
+        GD.Print("teleporter knows network is ready");
 
-        if (!isServer)
-        {
-            Connect("body_entered", this, nameof(OnPlayerEntered));
-        }
+        Connect("body_entered", this, nameof(OnPlayerEntered));
     }
 
     public void OnPlayerEntered(Node2D body)
     {
+        GD.Print("ping");
+
         if (body is PlayerNetworking)
         {
             PlayerNetworking p = (PlayerNetworking)body;
@@ -34,6 +33,8 @@ public class Teleporter : Area2D
             string[] requestHeaders = new string[1];
             requestHeaders[0] = $"Authorization: Bearer {pc.Token}";
 
+            GD.Print("teleporter should make request");
+
             HTTPRequest req = GetNode<HTTPRequest>("HTTPRequest");
             req.Connect("request_completed", this, nameof(DoChangeScene));
             req.Request($"http://127.0.0.1:8082/game/change_scene?targte_scene={TargetScene}", requestHeaders, false, HTTPClient.Method.Post, "");   
@@ -42,6 +43,8 @@ public class Teleporter : Area2D
 
     public void DoChangeScene(int result, int response_code, string[] headers, byte[] body)
     {
+            GD.Print("teleporter request complete");
+
             JSONParseResult json = JSON.Parse(Encoding.UTF8.GetString(body));
             Dictionary respData = (Dictionary)json.Result;
             string scene = (string)respData["scene"];
@@ -54,7 +57,5 @@ public class Teleporter : Area2D
             Node sceneNode = ps.Instance();
             GetTree().Root.AddChild(sceneNode);
             sceneNode.GetNode<PlayerClient>("/root/World/NetworkManager/PlayerClient").StartWithToken(pc.Token, ip, port);
-
-            GetTree().Root.GetNode<Node2D>("Menu").Free();
     }
 }
